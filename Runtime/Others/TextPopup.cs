23d8bb726cd67bf41856e1c8adebb9c281463fcc
Felipe Lopes Zem <felipelopeszem@hotmai.com>
2023-07-28 17:22:00 -0300

    gitignore fix and commented out a script that was using TMPro (because, for now, I'm not sure how to deal with it yet).

--- Runtime/Others/TextPopup.cs
+++ Runtime/Others/TextPopup.cs
@@ -1,36 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using TMPro;
 
-namespace ZemReusables
-{
-    public class TextPopup : MonoBehaviour
-    {
-        [Header("Manual references")]
-        [SerializeField] private TextMeshPro textMesh;
+//namespace ZemReusables
+//{
+//    public class TextPopup : MonoBehaviour
+//    {
+//        [Header("Manual references")]
+//        [SerializeField] private TextMeshPro textMesh;
 
-        [Header("Runtime")]
-        [SerializeField] private Transform followTarget;
+//        [Header("Runtime")]
+//        [SerializeField] private Transform followTarget;
 
-        private void Start()
-        {
-            Destroy(gameObject, 1F);
-        }
+//        private void Start()
+//        {
+//            Destroy(gameObject, 1F);
+//        }
 
-        private void LateUpdate()
-        {
-            Vector3 position = Vector3.up * Time.deltaTime;
-            textMesh.rectTransform.Translate(position);
-            if (followTarget) transform.position = followTarget.transform.position;
-        }
+//        private void LateUpdate()
+//        {
+//            Vector3 position = Vector3.up * Time.deltaTime;
+//            textMesh.rectTransform.Translate(position);
+//            if (followTarget) transform.position = followTarget.transform.position;
+//        }
 
-        public void Initialize(string text, Color color, Vector3 scale, Transform followTarget)
-        {
-            textMesh.SetText(text);
-            textMesh.color = color;
-            textMesh.rectTransform.localScale = scale;
-            this.followTarget = followTarget;
-        }
-    }
-}
+//        public void Initialize(string text, Color color, Vector3 scale, Transform followTarget)
+//        {
+//            textMesh.SetText(text);
+//            textMesh.color = color;
+//            textMesh.rectTransform.localScale = scale;
+//            this.followTarget = followTarget;
+//        }
+//    }
+//}
