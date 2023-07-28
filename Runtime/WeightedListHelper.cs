1e4d3b7b947f81b0f65c3c6755c01f87c45053b0
Felipe Lopes Zem <felipelopeszem@hotmai.com>
2023-07-28 18:53:53 -0300

    Added WeightedListHelper

---
+++ Runtime/WeightedListHelper.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ZemReusables
+{
+    public class WeightedListHelper
+    {
+        public T RandomPick<T>(IEnumerable<T> sequence, SeededRandom seededRandom, Func<T, float> weightSelector)
+        {
+            float totalWeight = sequence.Sum(weightSelector);
+            float targetWeight = seededRandom.NextFloat() * totalWeight;
+            float currentWeightIndex = 0;
+            foreach (var item in from weightedItem in sequence select new { Value = weightedItem, Weight = weightSelector(weightedItem) })
+            {
+                currentWeightIndex += item.Weight;
+                //If we've hit or passed the weight we are after for this item then it's the one we want.
+                //if a weight for an item is 0, then the item should never be selected.
+                //Using >= will give 0 weight items a very slim chance to be selected.
+                if (currentWeightIndex > targetWeight)
+                    return item.Value;
+            }
+            return default;
+        }
+    }
+}
